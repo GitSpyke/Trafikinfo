3861aca722eb77d6aeb624d3d6b37e3ba6398a7f
nordichat <alexander.salipur@gmail.com>
2021-03-29 12:13:18 +0200

    #Template OL

--- Trafikinfo/ClientApp/src/App.js
+++ Trafikinfo/ClientApp/src/App.js
@@ -4,8 +4,15 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-
 import './custom.css'
+//OpenLayers
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+
+
+
 
 export default class App extends Component {
   static displayName = App.name;
@@ -16,7 +23,12 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Map />
       </Layout>
     );
-  }
+    }
 }
+
+
+
+
